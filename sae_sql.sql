3e4318b786acff3a9e8ea0a2c6e07d8ff42f9448
Benjamin Jond <benjamin.jond@edu.univ-fcomte.fr>
2025-01-23 15:24:26 +0100

    SQL

--- sae_sql.sql
+++ sae_sql.sql
@@ -41,7 +41,8 @@ CREATE TABLE type_gant (
 CREATE TABLE taille (
                         id_taille INT PRIMARY KEY AUTO_INCREMENT,
                         num_taille_fr INT NOT NULL,
-                        taille_us VARCHAR(255) NOT NULL,
+                        taille_us_homme VARCHAR(255) NOT NULL,
+                        taille_us_femme VARCHAR(255) NOT NULL,
                         tour_de_main FLOAT NOT NULL
 );
 
@@ -56,6 +57,7 @@ CREATE TABLE gant (
                       type_gant_id INT NOT NULL,
                       fournisseur INT NOT NULL,
                       marque VARCHAR(255) NOT NULL,
+                      image VARCHAR(255) NOT NULL,
                       CONSTRAINT fk_gant_taille FOREIGN KEY (taille_id) REFERENCES taille(id_taille),
                       CONSTRAINT fk_gant_type_gant FOREIGN KEY (type_gant_id) REFERENCES type_gant(id_type_gant)
 
@@ -94,14 +96,54 @@ INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) 
 
 
 INSERT INTO etat(id_etat,libelle) VALUES
-                                        (1,'En attente'),
-                                        (2,'En cours de préparation'),
-                                        (3,'Expédiée'),
-                                        (4,'Livrée');
+                                      (1,'En attente'),
+                                      (2,'En cours de préparation'),
+                                      (3,'Expédiée'),
+                                      (4,'Livrée');
 
 INSERT INTO type_gant(id_type_gant,nom_type_gant) VALUES
-                                                    (NULL,'Boxe'),
-                                                    (NULL, 'Ski'),
-                                                    (NULL, 'Golf'),
-                                                    (NULL, 'Jaradinage');
+                                                      (NULL,'Boxe'),
+                                                      (NULL, 'Ski'),
+                                                      (NULL, 'Golf'),
+                                                      (NULL, 'Jaradinage');
+
+INSERT INTO taille(id_taille,num_taille_fr,taille_us_homme,taille_us_femme,tour_de_main) VALUES
+                                                                                             (1,6.5,'NULL','S',17.5),
+                                                                                             (2,7,'NULL','M',19),
+                                                                                             (3,7.5,'NULL','L',20),
+                                                                                             (4,8,'S','XL',21.5),
+                                                                                             (5,8.5,'M','XXL',23),
+                                                                                             (6,9,'L','NULL',24),
+                                                                                             (7,9.5,'XL','NULL',25.5),
+                                                                                             (8,10,'XXL','NULL',27);
+
+INSERT INTO gant(id_gant,nom_gant,poids,couleur,prix_gant,taille_id,type_gant_id,fournisseur,marque,image) VALUES
+                                                                                                               (NULL,'Gant de boxe en cuir','0.5','Noir','50','1','1','1','Everlast','gant1.jpg'),
+                                                                                                               (NULL,'Gant de boxe en cuir','0.5','Rouge','50','2','1','1','Everlast','gant2.jpg'),
+                                                                                                               (NULL,'Gant de boxe en cuir','0.5','Bleu','50','3','1','1','Everlast','gant3.jpg'),
+                                                                                                               (NULL,'Gant de boxe en cuir','0.5','Blanc','50','4','1','1','Everlast','gant4.jpg'),
+                                                                                                               (NULL,'Gant de boxe en cuir','0.5','Noir','50','5','1','1','Everlast','gant5.jpg');
+
+INSERT INTO commande(id_commande,date_achat,utilisateur_id,etat_id) VALUES
+                                                                      (1,'2020-01-01','2','1');
+
+INSERT INTO ligne_commande(commande_id,gant_id,prix,quantite) VALUES
+                                                                  (1,1,'50','1'),
+                                                                  (1,2,'50','1'),
+                                                                  (1,3,'50','1'),
+                                                                  (1,4,'50','1'),
+                                                                  (1,5,'50','1');
+
+INSERT INTO ligne_panier(utilisateur_id,gant_id,quantite,date_ajout) VALUES
+                                                                         (2,1,'1','2020-01-01'),
+                                                                         (2,2,'1','2020-01-01'),
+                                                                         (2,3,'1','2020-01-01'),
+                                                                         (2,4,'1','2020-01-01'),
+                                                                         (2,5,'1','2020-01-01');
+
+
+
+
+
+
 
