d5086b1fcd38a6769e63ad9cf14d07d71adafc31
Benjamin Jond <benjamin.jond@edu.univ-fcomte.fr>
2025-01-31 10:45:17 +0100

    test

--- controllers/client_coordonnee.py
+++ controllers/client_coordonnee.py
@@ -61,9 +61,13 @@ def client_coordonnee_delete_adresse():
 def client_coordonnee_add_adresse():
     mycursor = get_db().cursor()
     id_client = session['id_user']
+    sql='''SELECT * FROM utilisateur WHERE id_utilisateur=%s'''
+    mycursor.execute(sql,(id_client,))
+
+    utilisateur = mycursor.fetchone()
 
     return render_template('client/coordonnee/add_adresse.html'
-                           #,utilisateur=utilisateur
+                           ,utilisateur=utilisateur
                            )
 
 @client_coordonnee.route('/client/coordonnee/add_adresse',methods=['POST'])
