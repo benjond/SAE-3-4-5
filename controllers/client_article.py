4ea812825bed4f38502b8c0b03e3868746e9b8d1
Benjamin Jond <benjamin.jond@edu.univ-fcomte.fr>
2025-02-28 19:12:01 +0100

    FIltre fonctionnel on touche plus au filtre

--- controllers/client_article.py
+++ controllers/client_article.py
@@ -1,77 +1,77 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
-from flask import Blueprint
-from flask import request, render_template, session
-
+from flask import Blueprint, request, render_template, session
 from connexion_db import get_db
 
-client_article = Blueprint('client_article', __name__,
-                        template_folder='templates')
+client_article = Blueprint('client_article', __name__, template_folder='templates')
 
 @client_article.route('/client/index')
-@client_article.route('/client/article/show')              # remplace /client
-def client_article_show():                                 # remplace client_index
+@client_article.route('/client/article/show')
+def client_article_show():
     mycursor = get_db().cursor()
-    id_client = session['id_user']
-    list_param = []
-    condition_and = ""
-    if 'filtre_nom' in request.args:
-        filtre_nom = request.args['filtre_nom']
-        condition_and += " AND nom_gant LIKE %s"
-        list_param.append(f"%{filtre_nom}%")
-    # utilisation du filtre
-    # prise en compte des commentaires et des notes dans le SQL
-    sql = ''' SELECT id_gant as id_article
-                    , nom_gant as nom
-                    , prix_gant as prix
-                    , stock as stock
-                    , image as image
-                FROM gant
-                ORDER BY nom_gant;
-            '''
-    mycursor.execute(sql)
-    gant = mycursor.fetchall()
-    articles = gant
-            
+    id_client = session.get('id_user')
 
-    # pour le filtre
+    condition_and = []
+    list_param = []
+
+    sql = '''SELECT gant.id_gant as id_article, gant.nom_gant as nom, gant.prix_gant as prix,
+                    gant.stock as stock, gant.image as image, type_gant.nom_type_gant as type
+             FROM gant
+             JOIN type_gant ON gant.type_gant_id = type_gant.id_type_gant
+             JOIN taille ON gant.taille_id = taille.id_taille'''
+
+    if "filter_word" in session and session["filter_word"]:
+        condition_and.append("gant.nom_gant LIKE %s")
+        list_param.append(f"%{session['filter_word']}%")
+
+    if "filter_prix_min" in session and session["filter_prix_min"]:
+        condition_and.append("gant.prix_gant >= %s")
+        list_param.append(session["filter_prix_min"])
+
+    if "filter_prix_max" in session and session["filter_prix_max"]:
+        condition_and.append("gant.prix_gant <= %s")
+        list_param.append(session["filter_prix_max"])
+
+    if "filter_types" in session and session["filter_types"]:
+        placeholders = ', '.join(['%s'] * len(session["filter_types"]))
+        condition_and.append(f"gant.type_gant_id IN ({placeholders})")
+        list_param.extend(session["filter_types"])
+
+    if condition_and:
+        sql += " WHERE " + " AND ".join(condition_and)
+
+    sql += " ORDER BY gant.nom_gant;"
+    
+    mycursor.execute(sql, tuple(list_param))
+    articles = mycursor.fetchall()
+
     sql = '''SELECT id_type_gant as id_type_article, nom_type_gant AS libelle FROM type_gant ORDER BY nom_type_gant;'''
     mycursor.execute(sql)
     types_article = mycursor.fetchall()
-        
 
-
-    # pour le panier
-    sql = '''SELECT gant.id_gant as id_article
-                    , gant.nom_gant as nom
-                    , gant.prix_gant as prix
-                    , gant.stock as stock
-                    , gant.image as image
-                    , ligne_panier.quantite as quantite
-                FROM gant
-                JOIN ligne_panier ON gant.id_gant = ligne_panier.gant_id
-                WHERE ligne_panier.utilisateur_id = %s
-                ORDER BY gant.nom_gant;
-    '''
-
+    sql = '''SELECT gant.id_gant as id_article, gant.nom_gant as nom, gant.prix_gant as prix,
+                    gant.stock as stock, gant.image as image, ligne_panier.quantite as quantite
+             FROM gant
+             JOIN ligne_panier ON gant.id_gant = ligne_panier.gant_id
+             WHERE ligne_panier.utilisateur_id = %s
+             ORDER BY gant.nom_gant;'''
+    
     mycursor.execute(sql, (id_client,))
     articles_panier = mycursor.fetchall()
 
     prix_total = None
-    if len(articles_panier) >= 1:
-        # calcul du prix total du panier
-        pass
+    if articles_panier:
+        sql = '''SELECT SUM(gant.prix_gant * ligne_panier.quantite) AS prix_total_panier 
+                 FROM ligne_panier 
+                 JOIN gant ON ligne_panier.gant_id = gant.id_gant
+                 WHERE ligne_panier.utilisateur_id = %s;'''
+        mycursor.execute(sql, (id_client,))
+        prix_total = mycursor.fetchone()["prix_total_panier"]
 
-    # utilise les articles filtées de la session si il sont disponible
-    if 'articles_filter' in session and session['articles_filter']:
-        articles = session['articles_filter']
-        items_filtre = session.get('items_filtre') or []
-    else:
-        items_filtre = types_article
+    items_filtre = session.get('items_filtre', types_article)
 
-    return render_template('client/boutique/panier_article.html'
-                           , articles=articles
-                           , articles_panier=articles_panier
-                           , prix_total=prix_total
-                           , items_filtre=items_filtre
-                           )
+    return render_template('client/boutique/panier_article.html',
+                           articles=articles,
+                           articles_panier=articles_panier,
+                           prix_total=prix_total,
+                           items_filtre=items_filtre)
--- controllers/client_panier.py
+++ controllers/client_panier.py
@@ -1,29 +1,22 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
-from flask import Blueprint
-from flask import request, render_template, redirect, abort, flash, session
-
+from flask import Blueprint, request, render_template, redirect, flash, session
 from connexion_db import get_db
 
-client_panier = Blueprint('client_panier', __name__,
-                        template_folder='templates')
-
+client_panier = Blueprint('client_panier', __name__, template_folder='templates')
 
 @client_panier.route('/client/panier/add', methods=['POST'])
 def client_panier_add():
     mycursor = get_db().cursor()
-    id_client = session['id_user']
+    id_client = session.get('id_user')
     id_article = request.form.get('id_article')
     quantite = int(request.form.get('quantite'))
 
-    sql = '''SELECT * FROM ligne_panier WHERE gant_id = %s AND utilisateur_id = %s'''
-    mycursor.execute(sql, (id_article, id_client))
-    article_panier = mycursor.fetchone()
-
-    mycursor.execute("SELECT * FROM gant WHERE id_gant = %s", (id_article,))
+    sql = '''SELECT stock FROM gant WHERE id_gant = %s'''
+    mycursor.execute(sql, (id_article,))
     article = mycursor.fetchone()
 
-    if article is None:
+    if not article:
         flash("Article non trouvé", "error")
         return redirect('/client/article/show')
 
@@ -31,11 +24,16 @@ def client_panier_add():
         flash("Stock insuffisant", "error")
         return redirect('/client/article/show')
 
-    if article_panier:
-        sql = "UPDATE ligne_panier SET quantite = quantite + %s WHERE utilisateur_id = %s AND gant_id = %s"
+    sql = '''SELECT quantite FROM ligne_panier WHERE utilisateur_id = %s AND gant_id = %s'''
+    mycursor.execute(sql, (id_client, id_article))
+    existing_item = mycursor.fetchone()
+
+    if existing_item:
+        sql = '''UPDATE ligne_panier SET quantite = quantite + %s WHERE utilisateur_id = %s AND gant_id = %s'''
         mycursor.execute(sql, (quantite, id_client, id_article))
     else:
-        sql = "INSERT INTO ligne_panier(utilisateur_id, gant_id, quantite, date_ajout) VALUES (%s, %s, %s, current_timestamp)"
+        sql = '''INSERT INTO ligne_panier(utilisateur_id, gant_id, quantite, date_ajout) 
+                 VALUES (%s, %s, %s, current_timestamp)'''
         mycursor.execute(sql, (id_client, id_article, quantite))
 
     sql = "UPDATE gant SET stock = stock - %s WHERE id_gant = %s"
@@ -47,27 +45,14 @@ def client_panier_add():
 @client_panier.route('/client/panier/delete', methods=['POST'])
 def client_panier_delete():
     mycursor = get_db().cursor()
-    id_client = session['id_user']
-    id_article = request.form.get('id_article', '')
-    quantite = 1
+    id_client = session.get('id_user')
+    id_article = request.form.get('id_article')
+    
+    sql = '''DELETE FROM ligne_panier WHERE utilisateur_id = %s AND gant_id = %s'''
+    mycursor.execute(sql, (id_client, id_article))
 
-    # Selection de la ligne du panier pour l'article et l'utilisateur connecté
-    sql = '''SELECT * FROM ligne_panier WHERE gant_id = %s AND utilisateur_id = %s'''
-    mycursor.execute(sql, (id_article, id_client))
-    article_panier = mycursor.fetchone()
-
-    if article_panier and article_panier['quantite'] > 1:
-        # Mise à jour de la quantité dans le panier => -1 article
-        sql = '''UPDATE ligne_panier SET quantite = quantite - %s WHERE utilisateur_id = %s AND gant_id = %s'''
-        mycursor.execute(sql, (quantite, id_client, id_article))
-    else:
-        # Suppression de la ligne de panier
-        sql = '''DELETE FROM ligne_panier WHERE utilisateur_id = %s AND gant_id = %s'''
-        mycursor.execute(sql, (id_client, id_article))
-
-    # Mise à jour du stock de l'article disponible
     sql = '''UPDATE gant SET stock = stock + %s WHERE id_gant = %s'''
-    mycursor.execute(sql, (quantite, id_article))
+    mycursor.execute(sql, (1, id_article))
 
     get_db().commit()
     return redirect('/client/article/show')
@@ -75,168 +60,36 @@ def client_panier_delete():
 @client_panier.route('/client/panier/vider', methods=['POST'])
 def client_panier_vider():
     mycursor = get_db().cursor()
-    client_id = session['id_user']
+    id_client = session.get('id_user')
 
-    # Sélection des lignes de panier pour l'utilisateur connecté
-    sql = '''SELECT * FROM ligne_panier WHERE utilisateur_id = %s'''
-    mycursor.execute(sql, (client_id,))
+    sql = '''SELECT gant_id, quantite FROM ligne_panier WHERE utilisateur_id = %s'''
+    mycursor.execute(sql, (id_client,))
     items_panier = mycursor.fetchall()
 
     for item in items_panier:
-        # Suppression de la ligne de panier de l'article pour l'utilisateur connecté
-        sql = '''DELETE FROM ligne_panier WHERE utilisateur_id = %s AND gant_id = %s'''
-        mycursor.execute(sql, (client_id, item['gant_id']))
+        sql = '''UPDATE gant SET stock = stock + %s WHERE id_gant = %s'''
+        mycursor.execute(sql, (item['quantite'], item['gant_id']))
 
-        # Mise à jour du stock de l'article : stock = stock + qté de la ligne pour l'article
-        sql2 = '''UPDATE gant SET stock = stock + %s WHERE id_gant = %s'''
-        mycursor.execute(sql2, (item['quantite'], item['gant_id']))
+    sql = '''DELETE FROM ligne_panier WHERE utilisateur_id = %s'''
+    mycursor.execute(sql, (id_client,))
 
     get_db().commit()
     return redirect('/client/article/show')
 
-
-@client_panier.route('/client/panier/delete/line', methods=['POST'])
-def client_panier_delete_line():
-    mycursor = get_db().cursor()
-    id_client = session['id_user']
-    id_article = request.form.get('id_article')
-
-    # Sélection de la ligne du panier pour l'article et l'utilisateur connecté
-    sql = '''SELECT * FROM ligne_panier WHERE gant_id = %s AND utilisateur_id = %s'''
-    mycursor.execute(sql, (id_article, id_client))
-    article_panier = mycursor.fetchone()
-
-    if article_panier:
-        # Suppression de la ligne de panier
-        sql = '''DELETE FROM ligne_panier WHERE utilisateur_id = %s AND gant_id = %s'''
-        mycursor.execute(sql, (id_client, id_article))
-
-        # Mise à jour du stock de l'article : stock = stock + qté de la ligne pour l'article
-        sql2 = '''UPDATE gant SET stock = stock + %s WHERE id_gant = %s'''
-        mycursor.execute(sql2, (article_panier['quantite'], id_article))
-
-    get_db().commit()
-    return redirect('/client/article/show')
-
-
 @client_panier.route('/client/panier/filtre', methods=['POST'])
 def client_panier_filtre():
-    mycursor = get_db().cursor()
-    articles_filter = []
-    filter_word = request.form.get('filter_word', None)
-    filter_prix_min = request.form.get('filter_prix_min', None)
-    filter_prix_max = request.form.get('filter_prix_max', None)
-    filter_types = request.form.getlist('filter_types')
-
-    # Debug :
-    print(
-        f"filter_word : {filter_word} / {type(filter_word)}\n"
-        f"filter_prix_min : {filter_prix_min} / {type(filter_prix_min)}\n"
-        f"filter_prix_max : {filter_prix_max} / {type(filter_prix_max)}\n"
-        f"filter_types : {filter_types} / {type(filter_types)}\n"
-    )
-
-    # Test des variables puis mise en session des variables
-
-    # Word:
-    if filter_word is not None:
-        if len(filter_word) > 1:
-            if filter_word.isalpha():
-                session['filter_word'] = filter_word
-                sql1 = '''SELECT * FROM gant WHERE gant.nom_gant LIKE %s'''
-                mycursor.execute(sql1, ('%' + filter_word + '%',))
-                articles_filter = mycursor.fetchall()
-                print(f"Articles Filter Word : {articles_filter}") ## Debug
-            else:
-                flash(u"votre Mot rechercher doit uniquement être composé de lettres")
-        else:
-            if len(filter_word) == 1:
-                flash(u"votre Mot rechercher doit être composé de plus de 2 lettre")
-            else:
-                session.pop("filter_word", None)
-
-    # Prix:
-    if filter_prix_max:
-        session['filter_prix_max'] = filter_prix_max
-        if not filter_prix_min:
-            filter_prix_min = '0'
-            session['filter_prix_min'] = filter_prix_min
-        session['filter_prix_min'] = filter_prix_min
-        sql2 = ''' SELECT * FROM gant WHERE gant.prix_gant BETWEEN %s and %s'''
-        mycursor.execute(sql2, (int(filter_prix_min), int(filter_prix_max)))
-        articles_filter = mycursor.fetchall()
-
-    # Types:
-    if filter_types:
-        session['filter_types'] = filter_types
-        sql3 = '''SELECT * FROM gant INNER JOIN type_gant ON type_gant.id_type_gant = gant.type_gant_id WHERE type_gant.id_type_gant IN (%s)''' % ','.join(['%s'] * len(filter_types))
-        mycursor.execute(sql3, filter_types)
-        articles_filter = mycursor.fetchall()
-
-    # Mise à jour des lignes de panier en fonction des filtres
-    print(f"Article Filter : {articles_filter}") # DEBUG
-
-    session['articles_filter'] = articles_filter
-    session['items_filtre'] = [item['id_gant'] for item in articles_filter]
-    print(f"\n\n\nSession : {session}") # DEBUG
-
+    session['filter_word'] = request.form.get('filter_word', '').strip() or ''
+    session['filter_prix_min'] = request.form.get('filter_prix_min', '').strip() or ''
+    session['filter_prix_max'] = request.form.get('filter_prix_max', '').strip() or ''
+    session['filter_types'] = request.form.getlist('filter_types') or ''
     return redirect('/client/article/show')
-
-
-"""
-@client_panier.route('/client/panier/filtre', methods=['POST'])
-def client_panier_filtre():
-    filter_word = request.form.get('filter_word', None)
-    filter_prix_min = request.form.get('filter_prix_min', None)
-    filter_prix_max = request.form.get('filter_prix_max', None)
-    filter_types = request.form.getlist('filter_types', None)
-    # test des variables puis
-    mycursor = get_db().cursor() 
-    sql = "SELECT * FROM article"
-    list_param = []
-    condition_and = ""
-    if "filter_word" in session or "filter_prix_min" in session or "filter_prix_max" in session or "filter_types" in session:
-        sql = sql + "WHERE"
-    if "filter_word" in session :
-        sql = sql + "nom LIKE %s"
-        list_param.append("%" + session["filter_word"] + "%")
-        condition_and = " AND "
-    if "filter_prix_min" in session or "filter_prix_max" in session :
-        sql = sql + condition_and + "prix BETWEEN %s and %s"
-        if "filter_prix_min" in session:
-            list_param.append(session["filter_prix_min"])
-        else:
-            list_param.append("0")
-        list_param.append(session["filter_prix_max"])
-        condition_and = "AND"
-    if "filter_types" in session :
-        sql = sql + condition_and + "("
-        last_item = session["filter_types"][-1]
-        for item in session["filter_types"]:
-            sql = sql + "type_article_id = %s"
-            if item != last_item:
-                sql = sql + "or"
-            list_param.append(item)
-        sql = sql + ")"
-    tuple_sql = tuple(list_param)
-    print(f"sql request : {sql}")
-    # mise en session des variables
-
-    print(f"\n\n\nSession : {session}")  # DEBUG
-    return redirect('/client/article/show')
-"""
-
-
 
 @client_panier.route('/client/panier/filtre/suppr', methods=['POST'])
 def client_panier_filtre_suppr():
-    # suppression  des variables en session
     session.pop('filter_word', None)
     session.pop('filter_prix_min', None)
     session.pop('filter_prix_max', None)
     session.pop('filter_types', None)
     session.pop('articles_filter', None)
     session.pop('items_filtre', None)
-    print("suppr filtre")
     return redirect('/client/article/show')
-
